6accc8a98b9b342ce8db9d7e3dc6dd0bed4016e9
Jason Cote <cotejason@gmail.com>
2018-02-19 08:40:39 -0600

    Made card image clickable

--- js/Card.jsx
+++ js/Card.jsx
@@ -6,11 +6,13 @@ const Card = (props) => {
   return (
     <div className="col-3 mb-4">
       <div className="card">
-        <img
-          className="card-img-top"
-          alt={`${props.show.name} Info`}
-          src={`/public/img/posters/${props.show.poster}`}
-        />
+        <a data-toggle="modal" data-target={`#${props.show.imdbID}`}>
+          <img
+            className="card-img-top"
+            alt={`${props.show.name} Info`}
+            src={`/public/img/posters/${props.show.poster}`}
+          />
+        </a>
         <div className="card-body">
           {/*<Link to={`/card/${props.show.imdbID}`}>See more</Link>*/}
           <h3 className="card-title">{props.show.title}</h3>
